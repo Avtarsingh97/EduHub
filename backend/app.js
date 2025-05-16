5b1a4dcb9979c993aa4ab600ac79cdfad3fba150
Avtar <officialavtar13@gmail.com>
2025-05-16 14:22:00 +0530

    update CORS

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ require("./config/db");
 const routes = require('./routes/v1');
 const config = require('./config');
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://eduhub-steel.vercel.app',
+    credentials: true, // if you're using cookies or headers like Authorization
+  }));
+
+  
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
