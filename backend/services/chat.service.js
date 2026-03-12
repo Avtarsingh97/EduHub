b8f0f9ff0e8481923ad1feb411585d0799cdc4a7
AVTAR SINGH <130889026+Avtarsingh97@users.noreply.github.com>
2026-03-12 18:11:48 +0530

    update chat service and package.json

--- backend/services/chat.service.js
+++ backend/services/chat.service.js
@@ -1,27 +1,26 @@
 const fs = require("fs");
-const { ChatOpenAI } = require("@langchain/openai");
+const path = require("path");
+
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
 const { PromptTemplate } = require("@langchain/core/prompts");
 const { RunnableSequence } = require("@langchain/core/runnables");
-const config = require("../config");
 
-const path = require("path");
+const config = require("../config");
 
 const fileData = fs.readFileSync(
   path.join(__dirname, "../dataSource.txt"),
   "utf-8"
 );
 
-const llm = new ChatOpenAI({
-  openAIApiKey: config.GROQ_API_KEY,
-  modelName: "llama3-8b-8192",
-  temperature: 0,
-  configuration: {
-    baseURL: "https://api.groq.com/openai/v1"
-  }
+const llm = new ChatGoogleGenerativeAI({
+  apiKey: config.GEMINI_API_KEY,
+  model: "gemini-1.5-flash",
+  temperature: 0
 });
 
 async function askQuestion(question) {
   try {
+
     const prompt = PromptTemplate.fromTemplate(`
 You are a knowledgeable assistant.
 
@@ -33,18 +32,18 @@ User Question:
 
 Instructions:
 - Answer only from the document
-- If not found, say "I couldn't find that information"
-- If greeting, greet back
+- If not found say "I couldn't find that information"
+- If greeting greet back
 - Be concise
 `);
 
     const chain = RunnableSequence.from([prompt, llm]);
 
     const response = await chain.invoke({
-      question: question,
+      question
     });
 
-    return response?.content || response?.text || "No response generated";
+    return response?.content || "No response generated";
 
   } catch (err) {
     console.error("Error:", err.message);
